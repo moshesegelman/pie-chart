124a6cdffdc10a5b07806746bcb0b644cd7e721b
moshesegelman <moshesegelman41@gmail.com>
2022-02-27 02:42:35 -0500

    adds users routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+      get "/users" => "users#index"
+      get "/users/:id" => "users#show"
+      post "/users" => "users#create"
+      patch "/users/:id" => "users#update"
+      delete "/users/:id" => "users#destroy"
+  end
 end
